7e0021336f7b85e3cc529e38af7de372da1964d8
irosikoni <irosikoni@gmail.com>
2022-10-03 22:15:56 +0200

    step 5 finished

---
+++ src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,9 @@
+package agh.ics.oop;
+import static java.lang.System.out;
+
+public class World {
+    public static void main(String[] args) {
+        out.println("Start");
+        out.println("Stop");
+    }
+}
