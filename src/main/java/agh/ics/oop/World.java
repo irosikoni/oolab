fa4ab953e39e19ca0ace771908186d171c15f474
irosikoni <irosikoni@gmail.com>
2022-10-04 10:38:38 +0200

    step 9 finished

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -2,8 +2,12 @@ package agh.ics.oop;
 import static java.lang.System.out;
 
 public class World {
+    static void run() {
+        out.println("Zwierzak idzie do przodu");
+    }
     public static void main(String[] args) {
         out.println("Start");
+        run();
         out.println("Stop");
     }
 }
