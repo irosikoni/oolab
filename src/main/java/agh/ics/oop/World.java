7bd81e340bc3b77bbfecf098b5786e7388a6b583
irosikoni <irosikoni@gmail.com>
2022-10-07 19:05:25 +0200

    lab1 finished

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -2,14 +2,38 @@ package agh.ics.oop;
 import static java.lang.System.out;
 
 public class World {
-    static void run(String[] args) {
-        out.println("Zwierzak idzie do przodu");
-        String newList = String.join(", ", args);
-        out.println(newList);
+    static Direction[] change(String[] args) {
+        int n = args.length;
+        Direction directions[] = new Direction[n];
+        for (int i = 0; i < n; i++) {
+            Direction message = switch (args[i]) {
+                case "f" -> Direction.FORWARD;
+                case "b" -> Direction.BACKWARD;
+                case "r" -> Direction.RIGHT;
+                case "l" -> Direction.LEFT;
+                default -> Direction.UNKNOWN;
+            };
+            directions[i] = message;
+        }
+        return directions;
+    }
+    static void run(Direction[] args) {
+        for (Direction move : args) {
+            String message = switch (move) {
+                case FORWARD -> "do przodu";
+                case BACKWARD -> "do tyłu";
+                case RIGHT -> "w prawo";
+                case LEFT -> "w lewo";
+                case UNKNOWN -> "nieznana komenda";
+            };
+            out.println("Zwierzak idzie " + message);
+
+        }
     }
     public static void main(String[] args) {
         out.println("Start");
-        run(args);
+        Direction dir[] = change(args);
+        run(dir);
         out.println("Stop");
     }
 }
