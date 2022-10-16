afe7485aac16b8fdd5861c7060e98c2f438c2806
irosikoni <irosikoni@gmail.com>
2022-10-16 14:54:51 +0200

    lab2 final fixes

--- src/main/java/agh/ics/oop/MapDirection.java
+++ src/main/java/agh/ics/oop/MapDirection.java
@@ -7,43 +7,39 @@ public enum MapDirection {
     EAST;
 
     public String toString() {
-        String newDirection = switch (this) {
+        return switch (this) {
             case EAST -> "Wschód";
             case WEST -> "Zachód";
             case NORTH -> "Północ";
             case SOUTH -> "Południe";
         };
-        return newDirection;
     }
 
     public MapDirection next() {
-        MapDirection newDirection1 = switch (this) {
+        return switch (this) {
             case EAST -> SOUTH;
             case WEST -> NORTH;
             case NORTH -> EAST;
             case SOUTH -> WEST;
         };
-        return newDirection1;
     }
 
     public MapDirection previous() {
-        MapDirection newDirection2 = switch (this) {
+        return switch (this) {
             case EAST -> NORTH;
             case WEST -> SOUTH;
             case NORTH -> WEST;
             case SOUTH -> EAST;
         };
-        return newDirection2;
     }
 
     public Vector2d toUnitVector() {
-        Vector2d unitVector = switch (this) {
+        return switch (this) {
             case EAST -> new Vector2d(1, 0);
             case WEST -> new Vector2d(-1, 0);
             case NORTH -> new Vector2d(0, 1);
             case SOUTH -> new Vector2d(0, -1);
         };
-        return unitVector;
     }
 
 }
